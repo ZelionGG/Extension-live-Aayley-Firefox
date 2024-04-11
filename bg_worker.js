c12c859d0768cc9aae848f6c7c7df843692d969a
Aurélien ALVES <aurelien.alves@lanteas.com>
2024-04-11 13:22:45 +0200

    Uniformisation des extensions chrome et firefox (manifest v2 pour le moment).

--- bg_worker.js
+++ bg_worker.js
@@ -1,13 +1,13 @@
 const clientId = '7sjt9diwcoz210sdkc9xkokrm8h2ol';
-const clientSecret = 'new38yo7mh9tc2d6huuxlip1f24ali';
-let accessToken;
-let url = 'https://www.twitch.tv/aayley';
+let streamerName = 'monodie';
+let url = 'https://www.twitch.tv/' + streamerName;
 
 browser.alarms.create({ periodInMinutes: 1 });
 browser.alarms.onAlarm.addListener(getLiveStatus);
 
 async function getLiveStatus() {
     const isLiveTwitch = await getTwitchStatus();
+    console.log(isLiveTwitch);
 
     if (!isLiveTwitch)
         return false;
@@ -17,32 +17,35 @@ async function getLiveStatus() {
 
 async function getTwitchStatus() {
     try {
-        const response = await fetch('https://api.twitch.tv/helix/streams?user_login=skyyart', {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${accessToken}`,
-                'Client-ID': clientId
-            }
+        var raw = JSON.stringify({
+            "channels": [
+                streamerName
+            ]
         });
+
+        const response = await fetch('https://twitch.theorycraft.gg/channel-status', {
+            method: 'POST',
+            headers: {
+                'Client-ID': clientId,
+            },
+            body: raw
+        });
+
         if (!response.ok) {
-            if (response.status === 401) {
-                getTwitchToken();
-                return false;
-            }
             throw new Error(`Error! Status: ${response.status}`);
         }
         const data = await response.json();
-        const streamData = data.data[0];
+        const streamData = data[streamerName];
         if (streamData && streamData.type === 'live') {
-            if (sessionStorage.getItem("notification") !== "true")
+            if (this.notification != true)
                 browser.notifications.create('notifyON Aayley', { type: "basic", title: 'Aayley', message: 'Aayley est en live', iconUrl: "images/iconon128.png" }, function (id) { });
 
-            sessionStorage.setItem("notification", "true");
-            browser.action.setIcon({ path: "images/iconon48.png" });
+            this.notification = true;
+            browser.browserAction.setIcon({ path: "images/iconon48.png" });
             return true;
         } else {
-            sessionStorage.setItem("notification", "false");
-            browser.action.setIcon({ path: "images/iconoff48.png" });
+            this.notification = false;
+            browser.browserAction.setIcon({ path: "images/iconoff48.png" });
         }
     } catch (error) {
         console.log(error);
@@ -50,34 +53,19 @@ async function getTwitchStatus() {
     return false;
 }
 
-async function getTwitchToken() {
-    try {
-        const response = await fetch('https://id.twitch.tv/oauth2/token', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: `client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`,
-        });
-        if (!response.ok) {
-            throw new Error(`Error! Status: ${response.status}`);
-        }
-        const data = await response.json();
-        accessToken = data.access_token;
-    } catch (error) {
-        console.log(error);
-    }
-    getLiveStatus();
-}
-
 browser.notifications.onClicked.addListener(function (notificationId) {
     if (notificationId === 'notifyON Aayley') {
         browser.tabs.create({ url: url }, function (tab) { });
+        browser.notifications.clear("notifyON Aayley");
     }
 });
 
-browser.action.onClicked.addListener(() => {
+browser.browserAction.onClicked.addListener(async () => {
     browser.tabs.create({ url });
 });
+
+const keepAlive = () => setInterval(browser.runtime.getPlatformInfo, 20e3);
+browser.runtime.onStartup.addListener(keepAlive);
+keepAlive();
 
 getLiveStatus();
